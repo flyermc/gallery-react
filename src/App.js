240ace2dbf2f62cb057654e50a095cee606c04a9
Denis Sipakov <mc.flyer@gmail.com>
2020-01-19 10:57:41 +0300

    Test with two images

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Image image_src="https://lh3.googleusercontent.com/-LbvS6uL7FQw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rcw6rdu5ZUEoC_3pbTzOQRDr6B5Jg.CMID/s64-c/photo.jpg"/>
+          <Image image_src="https://lh3.googleusercontent.com/-LbvS6uL7FQw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rcw6rdu5ZUEoC_3pbTzOQRDr6B5Jg.CMID/s64-c/photo.jpg"/>
+          <Image image_src="https://lh3.googleusercontent.com/-LbvS6uL7FQw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rcw6rdu5ZUEoC_3pbTzOQRDr6B5Jg.CMID/s64-c/photo.jp" />
       </header>
     </div>
   );
