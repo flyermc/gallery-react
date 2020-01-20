aa38dc448feb06dd28e84a4a62ab282eab06d259
Denis Sipakov <mc.flyer@gmail.com>
2020-01-20 18:10:38 +0300

    Add store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
+
+import { Provider } from 'react-redux';
 import { Image } from './components/image';
 
+import store from './store/store';
+
 function App() {
-  return (
+    return (
+            <Provider store={store}>
     <div className="App">
       <header className="App-header">
           <Image image_src="https://lh3.googleusercontent.com/-LbvS6uL7FQw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rcw6rdu5ZUEoC_3pbTzOQRDr6B5Jg.CMID/s64-c/photo.jpg"/>
           <Image image_src="https://lh3.googleusercontent.com/-LbvS6uL7FQw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rcw6rdu5ZUEoC_3pbTzOQRDr6B5Jg.CMID/s64-c/photo.jp" />
       </header>
-    </div>
+            </div>
+            </Provider>
   );
 }
 
