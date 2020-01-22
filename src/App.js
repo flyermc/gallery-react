9ea25d1e7e280f78e77e19a9880194c5de1da4c1
Denis Sipakov <mc.flyer@gmail.com>
2020-01-22 16:12:05 +0300

    POC dispatch action

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import { Provider } from 'react-redux';
 import { Image } from './components/image';
 
 import configureStore from './store/store';
+import { createStore } from 'redux';
+import galleryReducer from './store/reducers';
 
 const store = configureStore();
+const store1 = createStore(galleryReducer);
 
 function App() {
     return (
@@ -14,7 +17,7 @@ function App() {
     <div className="App">
       <header className="App-header">
           <Image imageSrc="https://lh3.googleusercontent.com/-LbvS6uL7FQw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rcw6rdu5ZUEoC_3pbTzOQRDr6B5Jg.CMID/s64-c/photo.jpg"/>
-          <Image imageSrc="https://lh3.googleusercontent.com/-LbvS6uL7FQw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rcw6rdu5ZUEoC_3pbTzOQRDr6B5Jg.CMID/s64-c/photo.jp" />
+          <Image imageSrc="https://lh3.googleusercontent.com/-LbvS6uL7FQw/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rcw6rdu5ZUEoC_3pbTzOQRDr6B5Jg.CMID/s64-c/photo.jpg" />
       </header>
             </div>
             </Provider>
--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -3,19 +3,22 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { openImage } from '../../store/actions';
 
+const clickImage = (imageSrc) => (openImage(imageSrc));
+
 export const Image = ({imageSrc}) => {
     return (
-            <img src={imageSrc}></img>
+            <img src={imageSrc} onClick={(imageSrc) => clickImage(imageSrc)}></img>
     )
 }
+
 const mapDispatchToProps = (dispatch) => {
     return {
-        openImage: (data) => dispatch(openImage(data)),
+        clickImage: (data) => dispatch(openImage(data)),
     }
 }
 Image.propTypes = {
     imageSrc: PropTypes.string,
-    openImage: PropTypes.func.isRequired,
+    clickImage: PropTypes.func.isRequired,
 }
 
 export default connect(null, mapDispatchToProps)(Image);
