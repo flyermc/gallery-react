b27f8ee8e3703913d74bd2102fba90e4326dfe71
Denis Sipakov <mc.flyer@gmail.com>
2020-04-06 18:09:02 +0300

    Add saga

---
+++ src/sagas/saga.js
@@ -0,0 +1,18 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import * as APIService from '../services/galleryService';
+import IMAGES_REQUESTED from '../store/actions';
+
+function* fetchImages(action) {
+    try {
+       const images = yield call(APIService.getImages);
+       yield put({type: "IMAGES_FETCH_SUCCEEDED", user: user});
+    } catch (e) {
+       yield put({type: "IMAGES_FETCH_FAILED", message: e.message});
+    }
+ }
+
+ function* imagesSaga() {
+    yield takeLatest(IMAGES_REQUESTED, fetchImages);
+ }
+
+ export default imagesSaga;
