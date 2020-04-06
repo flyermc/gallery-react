625d4c8429f63552d267d459fddd6d1d0b7e3134
Denis Sipakov <mc.flyer@gmail.com>
2020-04-06 18:09:47 +0300

    Update actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,11 @@
 export const OPEN_IMAGE = 'OPEN_IMAGE';
 export const CLOSE_IMAGE = 'CLOSE_IMAGE';
+export const LOAD_IMAGES = 'LOAD_IMAGES';
+export const IMAGES_REQUESTED = 'IMAGES_REQUESTED';
+
+export function loadImages() {
+    return { type: LOAD_IMAGES }
+}
 
 export function openImage(data) {
     return { type: OPEN_IMAGE, data }
@@ -7,4 +13,8 @@ export function openImage(data) {
 
 export function closeImage() {
     return { type: CLOSE_IMAGE }
+}
+
+export function requestImages() {
+    return { type: IMAGES_REQUESTED }
 }
