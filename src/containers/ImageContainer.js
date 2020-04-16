ea6e8ee1792054e83a1ee20bb16f238621242fb8
Denis Sipakov <mc.flyer@gmail.com>
2020-04-16 17:31:29 +0300

    Read images from api

--- src/containers/ImageContainer.js
+++ src/containers/ImageContainer.js
@@ -16,13 +16,15 @@ const ImageContainer = ({ images, currentImage }) => {
     
     return (
         <Fragment>
-            { !currentImage &&
+            { !currentImage && images &&
               <Container>
-                <Image imageSrc="https://www.gstatic.com/images/branding/googlemic/2x/googlemic_color_24dp.png" />
-                <Image imageSrc="https://ssl.gstatic.com/play-apps-publisher-rapid/fox/ebdc0e5c50a832cc2c6baf79fab2585c/fox/gwt/970D5B88B010734097B54B173E4FC446.cache.svg" />
-                <Image imageSrc="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_dark_1x.png" />
-                <Image imageSrc="http://localhost:8010/media/CACHE/images/uploads/gallery/images/1564134619_0_2_3000_1690_600x0_80_0_0_9b0af28be1be2b76d43424f4c43_xmvSfa7/c4bc162982e5297589fc8a0a8b23e8af.png" />
-            </Container>}
+              { 
+                  images.map(({ thumbnail_image, uuid }) => (
+                    <Image imageSrc={thumbnail_image} />
+                  ))
+              }
+              </Container>
+            }
             { !!currentImage && <ImageDetails /> }
         </Fragment>
     )
