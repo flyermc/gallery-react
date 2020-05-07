3aa39f2a4825222709195c733c3ea16e913c6034
Denis Sipakov <mc.flyer@gmail.com>
2020-05-07 10:45:22 +0300

    Fixing counter

--- src/components/countdown/FormatedTime.js
+++ src/components/countdown/FormatedTime.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 export const FormatedTime = ({miliseconds}) => {
     const time = new Date(miliseconds);
     const day = Math.floor(miliseconds / (24 * 60 * 60 * 1000));
+    const hours = Math.floor((miliseconds / 1000 / 60 / 60) % 24);
+    const minutes = Math.floor((miliseconds / 1000 / 60) % 60);
+    const seconds = Math.floor((miliseconds / 1000) % 60);
     const formatTime = (time) => {
         let pad = time < 10 ? '0': '';
         return pad + time;
     }
     return (
         <span>
-            {day} days
-            {formatTime(time.getHours())}:
-            {formatTime(time.getMinutes())}:
-            {formatTime(time.getSeconds())}
+            {day} {day === 1 ? `day`: `days`}
+            {formatTime(hours)}:
+            {formatTime(minutes)}:
+            {formatTime(seconds)}
         </span>
     )
 }
