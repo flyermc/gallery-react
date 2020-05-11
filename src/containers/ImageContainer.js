252570b1cbeef594bc0686d89f0b6008d4e0ddf2
Denis Sipakov <mc.flyer@gmail.com>
2020-05-11 18:22:09 +0300

    Move counter logic into main container

--- src/containers/ImageContainer.js
+++ src/containers/ImageContainer.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { connect, useDispatch } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -9,17 +9,41 @@ import { imagesSelector, currentImageSelector } from '../selectors/imagesSelecto
 import { requestImages } from '../store/actions';
 
 import { Container, StyledItem } from './styled';
-import { FullScreenContainer } from '../components/image/styled';
+
+const DAY = 1000 * 60 * 60 * 24;
+const WEEK = DAY * 7;
+const PUBLISH_DAY = 6;
+const PUBLISH_HOUR = 10;
 
 const ImageContainer = ({ images, currentImage }) => {
     let dispatch = useDispatch();
     images || dispatch(requestImages());
 
+    var resultDate = new Date();
+    const now = new Date()
+
+    resultDate.setDate(now.getDate() + (WEEK + 2 - now.getDay()) % WEEK);
+    resultDate.setHours(PUBLISH_HOUR, 0, 0, 0);
+
+    const [counter, setCounter] = useState(0);
+
+    const updateTimer = useCallback(() => {
+        let diff = resultDate - new Date();
+        if (diff < 0) {
+            diff += WEEK;
+        }
+        setCounter(diff);
+    }, [resultDate]);
+
+    useEffect(() => {
+        setTimeout(() => updateTimer(), 1000);
+    }, [updateTimer]);
+
     return (
         <Fragment>
-            { images && <FullScreen imageSrc={images[0].photo} /> }
-        { images && false &&
-            <Container>
+            { images ? WEEK - counter <= DAY
+            ? <FullScreen imageSrc={images[0].photo} />
+            : <Container>
             {
                 images.map(({ photo_preview, uuid }, index) => (
                 <StyledItem number={index}>
@@ -28,7 +52,7 @@ const ImageContainer = ({ images, currentImage }) => {
                 ))
             }
             </Container>
-        }
+            : null}
         { !!currentImage && <ImageDetails image={currentImage} /> }
         <Countdown />
         </Fragment>
