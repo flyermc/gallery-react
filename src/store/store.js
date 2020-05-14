f67cf148d016c8a5c873efe06cf023e2d184b469
Denis Sipakov <mc.flyer@gmail.com>
2020-05-14 18:06:05 +0300

    Setup devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,36 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import galleryReducer from './reducers';
 import imagesSaga from '../sagas/saga';
-
 import createSagaMiddleware from 'redux-saga';
 
-const sagaMiddleware = createSagaMiddleware();
+const isDevMode = process.env.NODE_ENV !== 'production';
+
+const initStore = () => {
+    const sagaMiddleware = createSagaMiddleware();
+
+    let enhancer = null;
+
+    const enhancers = [applyMiddleware(sagaMiddleware)];
+
+    if (isDevMode && window.__REDUX_DEVTOOLS_EXTENSION__) {
+        enhancer = compose(
+            ...enhancers,
+            window.__REDUX_DEVTOOLS_EXTENSION__() // eslint-disable-line
+        );
+    } else {
+        enhancer = compose(...enhancers);
+    }
+
+    const initialState = {};
+
+    const store = createStore(galleryReducer, initialState, enhancer);
+
+    sagaMiddleware.run(imagesSaga);
+    return store;
+};
 
 function configureStore() {
-    const store = createStore(
-        galleryReducer,
-        composeWithDevTools(
-            applyMiddleware(sagaMiddleware),
-        ),
-    );
-    sagaMiddleware.run(imagesSaga);
-    
-    return store;
+    return initStore();
 }
+
 export default configureStore;
