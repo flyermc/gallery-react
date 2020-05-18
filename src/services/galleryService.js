3cd9714969943fc66e78d52418f95ac48605149e
Denis Sipakov <mc.flyer@gmail.com>
2020-05-18 15:33:42 +0300

    Add IP address request

--- src/services/galleryService.js
+++ src/services/galleryService.js
@@ -1,4 +1,10 @@
-const HOST = "http://localhost:8010/";
-const IMAGES_URL = HOST + "images/";
+const HOST = 'http://localhost:8010/';
+const IMAGES_URL = HOST + 'images/';
+const IP_ADDRESS_HOST = 'https://geoip-db.com/json';
 
 export const getImages = () => fetch(IMAGES_URL).then((data) => data.json());
+
+export const getIpAddress = () =>
+  fetch(IP_ADDRESS_HOST)
+    .then((res) => res.json())
+    .then((json) => json.IPv4);
