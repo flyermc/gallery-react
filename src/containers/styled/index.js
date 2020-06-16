999494dfc87f3e1f494930854a6fd411af22fac5
Denis Sipakov <mc.flyer@gmail.com>
2020-06-16 18:57:59 +0300

    UI changes

--- src/containers/styled/index.js
+++ src/containers/styled/index.js
@@ -4,10 +4,10 @@ export const Container = styled.div`
   display: grid;
   grid-auto-flow: row;
   max-width: 900px;
-  grid-template-columns: repeat(9, 1fr);
+  grid-template-columns: repeat(10, 1fr);
   grid-template-rows: repeat(6, 90px);
-  column-gap: 15px;
-  row-gap: 15px;
+  column-gap: 10px;
+  row-gap: 10px;
 `;
 
 export const StyledItem = styled.div`
@@ -17,29 +17,35 @@ export const StyledItem = styled.div`
     0: css`
       grid-column: 1/4;
       grid-row: 1/4;
-      border-radius: 7px;
+      border-radius: 2px;
     `,
     1: css`
       grid-column: 4/7;
       grid-row: 1/4;
-      border-radius: 7px;
+      border-radius: 2px;
     `,
     2: css`
-      grid-column: 7/10;
+      grid-column: 7/11;
       grid-row: 1/4;
-      border-radius: 7px;
+      border-radius: 2px;
     `,
     3: css`
       grid-column: 1/5;
       grid-row: 4/7;
       text-align: left;
-      border-radius: 7px;
+      border-radius: 2px;
     `,
     4: css`
-      grid-column: 5/10;
+      grid-column: 5/8;
       grid-row: 4/7;
       text-align: right;
-      border-radius: 7px;
+      border-radius: 2px;
+    `,
+    5: css`
+      grid-column: 8/11;
+      grid-row: 4/7;
+      text-align: right;
+      border-radius: 2px;
     `,
     })[number]
   };
