69717c2e117cb5bbecb99911fafb22bae11898a3
Denis Sipakov <mc.flyer@gmail.com>
2020-07-30 18:03:46 +0300

    Add like on fullscreen

--- src/components/image/FullScreen.js
+++ src/components/image/FullScreen.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getHotImageSelector } from '../../selectors/imagesSelector';
 
 import { requestHotImage } from '../../store/actions';
+import { Like } from './Like';
 
 import { FullScreenContainer } from './styled';
 
@@ -11,5 +12,9 @@ export const FullScreen = () => {
   const dispatch = useDispatch();
   const hotImage = useSelector(getHotImageSelector);
   dispatch(requestHotImage());
-  return <>{hotImage && <FullScreenContainer imageSrc={hotImage.photo} />}</>;
+  return (
+    <>
+      {hotImage && <FullScreenContainer imageSrc={hotImage.photo} />}
+      <Like imageUuid={hotImage.uuid} />
+    </>);
 };
