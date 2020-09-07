7dc82fe555369cdcc9837e3888520463ec2acbdb
Denis Sipakov <mc.flyer@gmail.com>
2020-09-07 11:40:48 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import { ImageContainer } from './containers';
 
 import configureStore from './store/store';
@@ -14,7 +15,12 @@ function App() {
         <Provider store={store}>
             <div className="App">
                 <header className="App-header">
-                    <ImageContainer />
+                  <Router>
+                    <Switch>
+                      <Route exact path="/" component={ImageContainer} />
+                      <Route component={ImageContainer} />
+                    </Switch>
+                  </Router>
                 </header>
             </div>
         </Provider>
