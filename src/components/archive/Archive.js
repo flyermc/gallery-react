0b4d7eda96be624d8fe24bb190053e5629e85b9e
Denis Sipakov <mc.flyer@gmail.com>
2020-09-11 12:55:55 +0300

    Update values in store

--- src/components/archive/Archive.js
+++ src/components/archive/Archive.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { useDispatch } from 'react-redux'
-import { StyledSidebar, StyledTitleItem, StyledItem, StyledCloseButton, StyledTitle, StyledSubTitle } from './styled'
-import { requestArchive } from '../../store/actions'
+import { StyledSidebar, StyledTitleItem, StyledItem, StyledCloseButton, StyledTitle, StyledSubTitle, StyledHambItem, StyledCurrentSelectedItem } from './styled'
+import { requestArchive, setArchiveYear, setArchiveMonth } from '../../store/actions'
 import MenuIcon from '../../icons/menu-icon.svg'
 import CloseIcon from '../../icons/close-icon.svg'
 
-const Calendar = () => {
+const Calendar = ({ onLoad }) => {
   const [year, setYear] = React.useState(null)
   const [month, setMonth] = React.useState(null)
   const dispatch = useDispatch()
 
   React.useEffect(() => {
     if (month && months.indexOf(month) >= 0) {
-      const mindex = months.indexOf(month) + 1
+      const mindex = (months.indexOf(month) + 1).toString()
+      setTimeout(() => {
+        onLoad(false)
+      }, 1000)
       dispatch(requestArchive({year, month: mindex}))
+        dispatch(setArchiveMonth(mindex))
     }
   // eslint-disable-next-line
   }, [month])
+
+  React.useEffect(() => {
+    dispatch(setArchiveYear(year))
+  }, [year])
 
   const years = ["2019", "2020"]
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -31,9 +39,9 @@ const Calendar = () => {
   return (
     <>
       {year && (
-        <StyledTitleItem onClick={() => setYear(null)}>
+        <StyledCurrentSelectedItem onClick={() => setYear(null)}>
           <StyledSubTitle>{year} {month ? month: null}</StyledSubTitle>
-        </StyledTitleItem>
+        </StyledCurrentSelectedItem>
       )}
       { year
           ? itemBlock(months, setMonth)
@@ -59,13 +67,13 @@ export const Archive = () => {
                 <StyledTitle>Archive</StyledTitle>
                 <StyledCloseButton src={CloseIcon} alt='Close' />
               </StyledTitleItem>
-              <Calendar />
+              <Calendar onLoad={setOpened} />
             </>
           )
           : (
-            <StyledTitleItem onClick={() => handleSidebar(!opened)}>
+            <StyledHambItem onClick={() => handleSidebar(!opened)}>
               <img src={MenuIcon} alt='Menu' />
-            </StyledTitleItem>
+            </StyledHambItem>
           )
       }
     </StyledSidebar>
