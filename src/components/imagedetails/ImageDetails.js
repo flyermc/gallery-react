60816417ea5a2a56164215865da8d03b1941b36e
Denis Sipakov <mc.flyer@gmail.com>
2020-09-25 15:24:42 +0300

    Add Loading panel

--- src/components/imagedetails/ImageDetails.js
+++ src/components/imagedetails/ImageDetails.js
@@ -1,14 +1,15 @@
 import React, { useRef, memo, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import { useClickOutsideHook } from '../../hooks/ClickOutsideHook';
 import { currentImageSelector } from '../../selectors/imagesSelector';
 import { getImageRequest } from '../../services/galleryService'
+import { Loading } from '../../components'
 
 import { Like } from './Like';
-import { ImageContainer, StyledImage, LoadingContainer } from './styled';
+import { ImageContainer, StyledImage } from './styled';
 
 export const ImageDetails = memo(({ id, like }) => {
   const likeRef = useRef(null);
@@ -35,16 +36,21 @@ export const ImageDetails = memo(({ id, like }) => {
 
   return (
     <>
-      { loading && <LoadingContainer>Loading...</LoadingContainer>  }
+      { loading && (
+        <ImageContainer loading={!loading}>
+          <Loading />
+        </ImageContainer>
+        )
+      }
       { image && (
-            <ImageContainer loading={loading}>
-              <StyledImage src={image.photo} alt='Loading...' onLoad={imageLoaded} />
-              <div ref={likeRef}>
-                <Like imageUuid={image.uuid} likeFrom={like} />
-              </div>
-            </ImageContainer>
-          )
-        }
+        <ImageContainer loading={loading}>
+          <StyledImage src={image.photo} alt='Loading...' onLoad={imageLoaded} />
+          <div ref={likeRef}>
+            <Like imageUuid={image.uuid} likeFrom={like} />
+          </div>
+        </ImageContainer>
+        )
+      }
     </>
   );
 });
