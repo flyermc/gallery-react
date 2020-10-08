3a0c47453be91707bf10e0dc3ca4e84d71eb2e5f
Denis Sipakov <mc.flyer@gmail.com>
2020-10-08 20:18:30 +0300

    Small changes

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,4 @@ export { Image, FullScreen } from './image';
 export { ImageDetails, Like } from './imagedetails';
 export { Countdown } from './countdown';
 export { Archive } from './archive';
-export { Loading } from './loading'
+export { Loading } from './loading';
